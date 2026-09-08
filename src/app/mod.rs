b52fb5d422d270677dd0e3f67241cb0c7588a131
Gabe Chantayan <garnet638@gmail.com>
2026-09-08 18:10:10 +0000

    Port global observer unregister

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -29,6 +29,7 @@ pub mod facade_for_selector;
 pub mod fixed3_assign;
 pub mod fixed_value;
 pub mod global_flag_table;
+pub mod global_observer_unregister;
 pub mod h264_decode_forwarder;
 pub mod iap_incoming_process_thread;
 pub mod iap_packet;
