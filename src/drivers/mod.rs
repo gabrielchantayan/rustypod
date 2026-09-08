028d1051ea48be8708e8c2b8b42e0b74c951106a
Gabe Chantayan <garnet638@gmail.com>
2026-09-08 17:29:11 +0000

    Port ATA PIO byte write

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,6 +1,7 @@
 //! Low-level hardware drivers (S5L8702 peripherals, MMIO).
 pub mod ata_cmd;
 pub mod ata_semaphore;
+pub mod ata_pio_write_byte;
 pub mod byte_identity_slot_init;
 pub mod bitmap;
 pub mod clock;
