bb8cbdc2831093b1001964f84d8ecacb3e0e6a60
Gabe Chantayan <garnet638@gmail.com>
2026-09-11 01:39:18 +0000

    Port clamped mode position

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -9,6 +9,7 @@ pub mod animation;
 pub mod artwork_slot_available;
 pub mod class_6800;
 pub mod class_8900;
+pub mod clamped_mode_position;
 pub mod class_8c00;
 pub mod buffer_refill_request;
 pub mod byte_source;
