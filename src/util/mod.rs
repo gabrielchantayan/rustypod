deefa4bc601104a0a8f6d2e2ff1a3411d37563c3
Gabe Chantayan <garnet638@gmail.com>
2026-09-11 19:34:57 +0000

    Port lazy slot table entry

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -38,6 +38,7 @@ pub mod is_tagged_c0;
 pub mod le_read;
 pub mod linked_list_count;
 pub mod list_find;
+pub mod lazy_slot_table_entry;
 pub mod ptr_vector;
 pub mod object_masked_word_refresh;
 pub mod object_word_payload_resolve;
