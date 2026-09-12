970ef9c16110c98c0874b5ee86b05cc765bfe46b
Gabe Chantayan <garnet638@gmail.com>
2026-09-12 18:35:09 +0000

    Port string object ordering comparator

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -86,6 +86,7 @@ pub mod stream_write_exact;
 pub mod stream_write_cstr;
 pub mod stream_write_owner_construct;
 pub mod string_object;
+pub mod string_object_less;
 pub mod string_from_vtable_slot_50;
 pub mod string_word_record;
 pub mod string_object_initial_sort_key;
