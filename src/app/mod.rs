2013b1fde8fc28878e10503741387b313264e0b6
Gabe Chantayan <garnet638@gmail.com>
2026-09-12 06:40:09 +0000

    Port selection position bound predicate

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -145,6 +145,7 @@ pub mod resource_chain;
 pub mod resource_reference_value;
 pub mod scoped_context;
 pub mod scoped_string_id_record_set;
+pub mod selection_position_at_or_past_item_count;
 pub mod service_manager;
 pub mod service_state_synchronize;
 pub mod service_handler_availability;
