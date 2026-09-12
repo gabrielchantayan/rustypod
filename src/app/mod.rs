223ff213fd03bca6664da4a5e96b28565f3c6bf7
Gabe Chantayan <garnet638@gmail.com>
2026-09-12 07:32:41 +0000

    Port dual-pool screen constructor

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -50,6 +50,7 @@ pub mod directory_iterator;
 pub mod context_scope_selector;
 pub mod current_record_handle;
 pub mod firmware_query_low_byte;
+pub mod dual_pool_screen;
 pub mod demo_mode_datetime;
 pub mod fallback_keyed_object;
 pub mod flag_2c_is_one;
