e3aa2255276ea4f9b8125d80fe5fe3a08b9e2cc5
Gabe Chantayan <garnet638@gmail.com>
2026-09-12 17:48:43 +0000

    Port streambuf slot consumption

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -79,6 +79,7 @@ pub mod string_vector_destruct;
 pub mod string_map;
 pub mod streambuf_slot_peek_equal;
 pub mod streambuf_slot_peek_byte;
+pub mod streambuf_slot_consume;
 pub mod stream_write;
 pub mod stream_write_exact;
 pub mod stream_write_cstr;
