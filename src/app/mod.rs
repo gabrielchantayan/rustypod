2c994543f96f89bfa552bc067765941fd9f8e588
Gabe Chantayan <garnet638@gmail.com>
2026-09-12 06:11:18 +0000

    Port global slot 5c dispatch

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -64,6 +64,7 @@ pub mod fixed_point;
 pub mod fixed_value;
 pub mod global_flag_table;
 pub mod global_identifier_slot;
+pub mod global_slot_5c_dispatch;
 pub mod global_observer_unregister;
 pub mod global_callback_unregister;
 pub mod global_callback_register;
