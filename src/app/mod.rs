6a4e8615268a994b02add64e6f71480af6f6ecfc
Gabe Chantayan <garnet638@gmail.com>
2026-09-13 23:55:45 +0000

    Port media player interface status

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -122,6 +122,7 @@ pub mod class_9400_current_item_matches_media_player;
 pub mod media_player_queue_refresh;
 pub mod media_player_pending_item;
 pub mod media_player_interface_slot_11c;
+pub mod media_player_interface_status;
 pub mod media_interface_slot_4c_result;
 pub mod metadata_record;
 pub mod indexed_slot_pending_reset;
