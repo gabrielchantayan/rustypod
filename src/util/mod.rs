7195fb088d35a95f831e1537dc6bb5ac0261c865
Gabe Chantayan <garnet638@gmail.com>
2026-09-13 05:37:31 +0000

    Port indexed record lookup

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -38,6 +38,7 @@ pub mod highest_set_bit;
 pub mod inner_state;
 pub mod i2s_transfer_slot;
 pub mod is_tagged_c0;
+pub mod indexed_record_lookup;
 pub mod le_read;
 pub mod linked_list_count;
 pub mod list_find;
