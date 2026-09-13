9b9060af3e432c09abca227a9bc060e0c8c8b928
Gabe Chantayan <garnet638@gmail.com>
2026-09-13 22:28:24 +0000

    Port two word clear constructor

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -120,6 +120,7 @@ pub mod string_encoding;
 pub mod templates;
 pub mod signed_key_tree_find;
 pub mod red_black_tree_increment;
+pub mod two_word_clear;
 pub mod three_word_clear;
 pub mod three_word_clear_alt;
 pub mod three_word_clear_third;
