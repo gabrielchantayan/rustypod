cbb81572510eedbf2f30f2226158f9264712bc25
Gabe Chantayan <garnet638@gmail.com>
2026-09-13 11:03:00 +0000

    Port indirect u32 equality helper

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -103,6 +103,7 @@ pub mod u16_le_store_last_byte;
 pub mod u16_pair_copy;
 pub mod u32_pair_store;
 pub mod u32_indirect_equal;
+pub mod u32_deref_equal;
 pub mod u32_le_store_last_byte;
 pub mod zero_u32_pair;
 pub mod zero_four_words;
