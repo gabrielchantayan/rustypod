60726cb596de947ba20d749db89e7799f3ed2b8c
Gabe Chantayan <garnet638@gmail.com>
2026-09-14 05:05:11 +0000

    Port base64 encoder

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -40,6 +40,7 @@
 //! [`bn_ucmp`] ports its unsigned magnitude comparison from the same
 //! file.
 pub mod add_lock;
+pub mod base64_encode;
 pub mod bio_printf;
 pub mod bio_new;
 pub mod bio_free;
