c41bedda0da29a2021d2128f0aa998915abd7e38
Gabe Chantayan <garnet638@gmail.com>
2026-09-15 20:43:24 +0000

    port object state initializer

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -71,6 +71,7 @@ pub mod context_scope;
 pub mod directory_iterator;
 pub mod context_scope_selector;
 pub mod current_record_handle;
+pub mod object_state_initialize;
 pub mod indexed_item_value;
 pub mod registration_handle_current_status;
 pub mod firmware_query_low_byte;
