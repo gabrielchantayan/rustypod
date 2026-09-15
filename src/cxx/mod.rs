078c02b46f250fb1507ad94f2b38a2a98b882086
Gabe Chantayan <garnet638@gmail.com>
2026-09-15 06:47:42 +0000

    Port RGBA8 cursor writer

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -15,6 +15,7 @@ pub mod clock_source_destroy;
 pub mod color_copy;
 pub mod color_pack;
 pub mod color_unpack;
+pub mod rgba8_cursor_write;
 pub mod rgba8_cursor_write_rgb555a1;
 pub mod rgba8_cursor_write_rgba4444;
 pub mod rgb8_cursor_write;
