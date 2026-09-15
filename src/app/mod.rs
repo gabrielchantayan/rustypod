7fb4b34e23f36c4b862b1445dfe512f188eb85b0
Gabe Chantayan <garnet638@gmail.com>
2026-09-15 21:34:21 +0000

    port parser character-class scan

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -304,6 +304,7 @@ pub mod timer_step_value;
 pub mod tuning_timer;
 pub mod task_registry;
 pub mod token_parser;
+pub mod character_class_scan;
 pub mod template_binding;
 pub mod tree_flag_propagation;
 pub mod tracker_record;
