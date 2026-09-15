80d3fe980eff92f3e02589404ba667eb40498c2d
Gabe Chantayan <garnet638@gmail.com>
2026-09-15 06:53:56 +0000

    Port xml input exhaustion check

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -161,4 +161,5 @@ pub mod xor_8d_in_place;
 pub mod xml_decode_codepoint_and_reset;
 pub mod xml_decode_skip_whitespace;
 pub mod xml_peek_skip_whitespace;
+pub mod xml_input_is_exhausted;
 pub mod xml_skip_whitespace;
