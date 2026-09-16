ce4c284aa69af96fe7fffbc435323dc869ef082e
Gabe Chantayan <garnet638@gmail.com>
2026-09-16 13:01:57 +0000

    Port FUN_083e72d4 as guarded_vtable_slot_1c_dispatch

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -250,6 +250,7 @@ pub mod vtable_slot_3c_result_u16;
 pub mod vtable_slot_2c_read_i16;
 pub mod vtable_slot_40_result_word;
 pub mod vtable_word_callback;
+pub mod guarded_vtable_slot_1c_dispatch;
 pub mod guarded_vtable_slot_40_dispatch;
 pub mod vtable_object_base_construct;
 pub mod vtable_two_pair_base_construct;
