d97b0534caa9730e8dd8f0f51c607f140db9a2dd
Gabe Chantayan <garnet638@gmail.com>
2026-09-17 08:51:56 +0000

    Port object flag 0x8 predicate

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -192,6 +192,7 @@ pub mod object_byte_at_5;
 pub mod object_bytes_64_or_6c_nonzero;
 pub mod object_offset_48;
 pub mod object_flag_nibble;
+pub mod object_flag_0x8_is_set;
 pub mod object_flag_0x100_is_set;
 pub mod object_owner_set;
 pub mod object_dispatch_entry;
