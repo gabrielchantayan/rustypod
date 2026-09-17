d28cae62cbf464d7dea6d5e0a35499e365c94625
Gabe Chantayan <garnet638@gmail.com>
2026-09-17 16:54:45 +0000

    Port opaque string owner destructor

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -122,6 +122,7 @@ pub mod opaque_context_initialize;
 pub mod opaque_header_literal_construct;
 pub mod opaque_layout_construct;
 pub mod opaque_pair_copy_from_word8;
+pub mod opaque_string_owner_destroy;
 pub mod opaque_four_word_copy_from_word7;
 pub mod opaque_header_payload_construct;
 pub mod opaque_context_destroy;
