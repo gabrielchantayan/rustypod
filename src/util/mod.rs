956b4f743a94285f472a310a8fc9e1ff67bf440c
Gabe Chantayan <garnet638@gmail.com>
2026-09-19 20:20:33 +0000

    port retail 080cfe50 veneer

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -196,6 +196,7 @@ pub mod str_fold_upper;
 pub mod strided_cursor_advance;
 pub mod table_find;
 pub mod tail_call_retail_080e76b8;
+pub mod tail_call_retail_080cfe50;
 pub mod table_transform_fixed_selector;
 pub mod text_layout_fit_glyph_count;
 pub mod tagged_counter;
