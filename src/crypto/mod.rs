137e4f8a8c96708c3b56ab6f976d8888dae068f2
Gabe Chantayan <garnet638@gmail.com>
2026-09-19 17:06:52 +0000

    Port BIO_push to Rust

--- src/crypto/mod.rs
+++ src/crypto/mod.rs
@@ -60,6 +60,7 @@ pub mod free_ex_data;
 pub mod new_ex_data;
 pub mod bio_snprintf;
 pub mod bio_ctrl;
+pub mod bio_push;
 pub mod bio_copy_next_retry;
 pub mod bn_num_bits;
 pub mod bn_num_bits_word;
