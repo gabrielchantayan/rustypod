5cfec0c7b476871360a97b2e7b323f17011d17ab
Gabe Chantayan <garnet638@gmail.com>
2026-09-20 07:30:31 +0000

    Port indexed element array release

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -141,6 +141,7 @@ pub mod opaque_observable_array_flag_construct;
 pub mod opaque_observable_array_payload_destroy;
 pub mod opaque_observable_array_dispose_elements;
 pub mod owned_element_array_release;
+pub mod indexed_element_array_release;
 pub mod owned_element_array_delete_cells;
 pub mod owned_string_owner_array_release;
 pub mod derived_observable_array_construct;
