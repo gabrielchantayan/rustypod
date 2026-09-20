4d8a965160d02290b26b3cdcf64b56ed7fe7d9ef
Gabe Chantayan <garnet638@gmail.com>
2026-09-20 21:44:52 +0000

    Port SQLite expression affinity comparison

--- src/sqlite/mod.rs
+++ src/sqlite/mod.rs
@@ -121,6 +121,7 @@ pub mod expire_prepared_statements;
 pub mod expr_affinity;
 pub mod expr_and;
 pub mod expr_coll_seq;
+pub mod expr_compare_affinity;
 pub mod expr_code;
 pub mod expr_code_and_cache;
 pub mod expr_code_compare;
