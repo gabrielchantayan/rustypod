d2ada47258253843c5d7ae3a9d28b4a298cdc81a
Gabe Chantayan <garnet638@gmail.com>
2026-09-20 15:06:06 +0000

    Port vtable slot release loop

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -248,6 +248,7 @@ pub mod video_engine;
 pub mod validator;
 pub mod vtable_query;
 pub mod vtable_slot_0x40_result_word;
+pub mod vtable_slot_40_release_each;
 pub mod wang_hash;
 pub mod hash_word;
 pub mod free_index_table_grow;
