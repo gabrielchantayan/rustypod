5e0d6f76e0368e50621be366ef0bd37b75cb4770
Gabe Chantayan <garnet638@gmail.com>
2026-09-20 18:40:12 +0000

    Port SQLite source cursor assignment

--- src/sqlite/mod.rs
+++ src/sqlite/mod.rs
@@ -210,6 +210,7 @@ pub mod select_height;
 pub mod select_dest_init;
 pub mod select_delete;
 pub mod src_list_delete;
+pub mod src_list_assign_cursors;
 pub mod src_list_from_table;
 pub mod src_list_lookup;
 pub mod set_string;
