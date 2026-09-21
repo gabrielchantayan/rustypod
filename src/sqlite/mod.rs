912923bcdee585dfd7ca9368bd6a98d497c5e295
Gabe Chantayan <garnet638@gmail.com>
2026-09-21 11:15:47 +0000

    Port SQLite overflow cell cleanup

--- src/sqlite/mod.rs
+++ src/sqlite/mod.rs
@@ -83,6 +83,7 @@ pub mod btree_cursor;
 pub mod btree_close_cursor;
 pub mod btree_clear_cursor;
 pub mod clear_page_overflow_cells;
+pub mod clear_overflow_cell;
 pub mod clear_cursor_saved_overflows;
 pub mod clear_saved_overflow;
 pub mod release_tracked_pair;
