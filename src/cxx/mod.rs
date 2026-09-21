8a41a056897dfb37e9d7bf4c379a1819dd9007c8
Gabe Chantayan <garnet638@gmail.com>
2026-09-21 19:42:34 +0000

    Port keyed record string match

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -228,6 +228,7 @@ pub mod string;
 pub mod filebuf_is_open;
 pub mod string_pair_word_uninitialized_copy;
 pub mod named_object_cache;
+pub mod keyed_record_find_string_match;
 pub mod string_range_destroy;
 pub mod string_vector_destruct;
 pub mod lazy_string_tables_initialize;
