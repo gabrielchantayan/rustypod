ca30fb8bd67a6578d809739f7b49318dc15284a7
Gabe Chantayan <garnet638@gmail.com>
2026-09-21 22:50:45 +0000

    Port selected entry range helper

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -182,6 +182,7 @@ pub mod selector_slot_address;
 pub mod scheduler_label_lookup;
 pub mod scheduler_handle_label_lookup;
 pub mod state_flags;
+pub mod selected_or_all_entry_range;
 pub mod short_filename_byte_is_rejected;
 pub mod scaled_word_list_from_i32;
 pub mod scoped_global_guard_destroy;
