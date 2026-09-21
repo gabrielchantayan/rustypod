357ebd0703cd19f6c45bc08e93c9339406d829b0
Gabe Chantayan <garnet638@gmail.com>
2026-09-21 21:23:32 +0000

    Port object word low-byte accessor

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -122,6 +122,7 @@ pub mod object_flags_take;
 pub mod object_flag_0x10_is_set;
 pub mod object_flag_status_code;
 pub mod object_byte_0x21;
+pub mod object_word_0x8_low_byte;
 pub mod object_set_byte_0x27;
 pub mod object_set_byte_0x18;
 pub mod object_set_word_0x40;
