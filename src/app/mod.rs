539edd2c602149bb3c285216c742a5a45c123ae6
Gabe Chantayan <garnet638@gmail.com>
2026-09-21 21:09:15 +0000

    Port fixed3 zero predicate

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -147,6 +147,7 @@ pub mod event_source;
 pub mod facade_registry_walk;
 pub mod facade_for_selector;
 pub mod fixed3_assign;
+pub mod fixed3_is_zero;
 pub mod transition_page_clone;
 pub mod fixed3_add_scaled;
 pub mod fixed3_difference;
