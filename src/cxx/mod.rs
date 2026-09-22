a1e9614f77a5cca9aed7e9cf42a8b3d9bae4ee7b
Gabe Chantayan <garnet638@gmail.com>
2026-09-22 10:31:14 +0000

    Port flagged pair constructor

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -68,6 +68,7 @@ pub mod empty_destructor_1e741c;
 pub mod empty_destructor_1d6030;
 pub mod empty_destructor_1d85d4;
 pub mod empty_destructor_1f3ff0;
+pub mod flagged_pair_construct;
 pub mod empty_destructor_08267b90;
 pub mod empty_destructor_080276b0;
 pub mod empty_destructor_08297568;
