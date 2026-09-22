f0c363f373875e4e5377f28fa951dc2580a3e8b7
Gabe Chantayan <garnet638@gmail.com>
2026-09-22 07:18:58 +0000

    port UTF-8 validator tail veneer

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -228,6 +228,7 @@ pub mod strided_cursor_advance;
 pub mod table_find;
 pub mod tail_call_retail_080e76b8;
 pub mod tail_call_retail_080cfe50;
+pub mod tail_call_retail_utf8_validate_three_byte;
 pub mod metadata_fetch_or_null;
 pub mod table_transform_fixed_selector;
 pub mod text_layout_fit_glyph_count;
