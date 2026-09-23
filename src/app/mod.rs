c3db4e4685e45e47b210c5fa3967ccd8dae9e1e8
Gabe Chantayan <garnet638@gmail.com>
2026-09-23 17:09:17 +0000

    Port six bit set state constructor

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -96,6 +96,7 @@ pub mod category_is_below_three;
 pub mod synchronized_selection_transition;
 pub mod flagged_base_construct;
 pub mod work_record_construct;
+pub mod six_bit_set_state_construct;
 pub mod callback_target_dispatch;
 pub mod callback_queue;
 pub mod callback_target_slot_14_dispatch;
