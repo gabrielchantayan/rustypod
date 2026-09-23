81aab3a8c9642e7ea32bf5f6a18b914b4e139e50
Gabe Chantayan <garnet638@gmail.com>
2026-09-23 18:40:31 +0000

    port empty destructor 08165bfc

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -81,6 +81,7 @@ pub mod empty_destructor_08267b90;
 pub mod empty_destructor_080276b0;
 pub mod empty_destructor_08297568;
 pub mod empty_destructor_166a3c;
+pub mod empty_destructor_165bfc;
 pub mod empty_destructor_1070b4;
 pub mod empty_destructor_083d8008;
 pub mod tracker_diagnostic_no_op;
