f1ab8f4a0b8c5d252188bb2448a97abebc08cb30
Gabe Chantayan <garnet638@gmail.com>
2026-09-24 03:01:52 +0000

    port ui notification state refresh

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -139,6 +139,7 @@ pub mod selection_clear_and_stop_timer;
 pub mod selection_schedule_timer;
 pub mod window_list_find_by_key;
 pub mod notification_dispatch;
+pub mod refresh_notification_state;
 pub mod record_lookup_value_word;
 pub mod input_state_transition;
 
