f25b6ecadc1b9db4d3d2bdaad5aced19728bf49d
Gabe Chantayan <garnet638@gmail.com>
2026-09-24 00:12:00 +0000

    Port retained value dispatch

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -124,6 +124,7 @@ pub mod descriptor_layout;
 pub mod descriptor_field;
 pub mod descriptor_attachment;
 pub mod callback_dispatch_release;
+pub mod value_dispatch_retain;
 pub mod callback_dispatch_has_pending;
 pub mod bool_message_dispatch;
 pub mod callback_owner_release;
