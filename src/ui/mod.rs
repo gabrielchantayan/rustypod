fa091e14f09bf9bdb7e2131c292d27a93ff7d565
Gabe Chantayan <garnet638@gmail.com>
2026-09-24 03:38:41 +0000

    port UI resource ordinal selector

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -46,6 +46,7 @@ pub mod render_owner_clip;
 pub mod render_request_from_descriptor;
 pub mod render_cell_state;
 pub mod object_resource_count;
+pub mod object_resource_max_ordinal_for_presence;
 pub mod query_object_display_name;
 pub mod object_resource_counted_string;
 pub mod object_resource_string;
