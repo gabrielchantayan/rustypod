a0bf08dba4f611bdce9810a248e0fa78f2d1be85
Gabe Chantayan <garnet638@gmail.com>
2026-09-25 17:52:53 +0000

    port word range loop entry 9318

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -50,6 +50,7 @@ pub mod copy_word_range_if_destination_tail_entry;
 pub mod copy_halfword_range_if_destination;
 pub mod copy_word_range_if_destination;
 pub mod copy_word_range_if_destination_loop_entry;
+pub mod copy_word_range_if_destination_loop_entry_9318;
 pub mod repeat_source_word_if_destination;
 pub mod fill_repeated_byte_if_destination;
 pub mod clone_16_byte_element_range;
