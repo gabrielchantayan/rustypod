68484f5dd503d1ddd8718182ff46f8e353994127
Gabe Chantayan <garnet638@gmail.com>
2026-09-26 07:09:41 +0000

    Port list node pool acquire at 083dd1a8

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -97,6 +97,7 @@ pub mod deque_front_advance_release;
 pub mod deque_front_dispatch;
 pub mod vector4_resize_fill;
 pub mod word_list_node_pool_acquire;
+pub mod list_node_pool_acquire_083dd1a8;
 pub mod list_node_pool_acquire_083dd2e4;
 pub mod list_node_pool_acquire_083dd4cc;
 pub mod list_node_pool_list_init;
