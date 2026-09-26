e4440d4c05d27e4fe71a60d29248621eb42c6fa4
Gabe Chantayan <garnet638@gmail.com>
2026-09-26 04:13:35 +0000

    Port deque_push_front_elem4

--- src/heap/mod.rs
+++ src/heap/mod.rs
@@ -5,6 +5,7 @@ pub mod alloc_core;
 pub mod block_deque;
 pub mod deque_construct_range_elem12;
 pub mod deque_push_back_elem4;
+pub mod deque_push_front_elem4;
 pub mod block_mgr;
 pub mod block_manager_mutex_lock;
 pub mod block_manager_mutex_unlock;
