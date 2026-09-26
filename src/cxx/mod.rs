2e0af511e803ab9f4b1a17ceb6064a01f5031e36
Gabe Chantayan <garnet638@gmail.com>
2026-09-26 21:55:04 +0000

    Port indexed element array destructor

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -12,6 +12,7 @@ pub mod callback_table_construct;
 pub mod container_item_count_or_zero;
 pub mod container_element_at_083d6818;
 pub mod indexed_element_array_destroy;
+pub mod indexed_element_array_destruct;
 pub mod container_first_node;
 pub mod container_first_node_083dbeb4;
 pub mod container_first_node_083dbdd4;
