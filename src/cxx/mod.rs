75fd8a22f503aa00bf4a6db216ed52dea31a0bd8
Gabe Chantayan <garnet638@gmail.com>
2026-09-26 15:15:34 +0000

    Port 12-byte conditional record copy

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -434,6 +434,7 @@ pub mod templates;
 pub mod container_dispose_elements;
 pub mod fixed_record_range_copy;
 pub mod copy_record_24_if_destination;
+pub mod copy_record_12_if_destination;
 pub mod signed_key_tree_find;
 pub mod equal_key_tree_clear;
 pub mod u32_map_value_slot;
