4ce9432b71da31ae62ea0471d47cd38e40aea434
Gabe Chantayan <garnet638@gmail.com>
2026-09-26 15:22:19 +0000

    port element array record copy construct

--- src/cxx/mod.rs
+++ src/cxx/mod.rs
@@ -72,6 +72,7 @@ pub mod clock_source_construct;
 pub mod clock_source_construct_zero;
 pub mod clock_source_destroy;
 pub mod copy_record_20_if_destination;
+pub mod element_array_record_copy_construct;
 pub mod copy_two_words_with_flag;
 pub mod copy_two_word_record_if_destination;
 pub mod record20_range_copy_if_destination;
